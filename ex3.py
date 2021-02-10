78991bd6136d64719e2a07a6a6f692c6397fdf60
polina-koval <78862292+polina-koval@users.noreply.github.com>
2021-02-10 16:54:56 +0200

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,3 +1,14 @@
+#Write a function called that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+#Examples
+#"()"              =>  true
+#")(()))"          =>  false
+#"("               =>  false
+#"(())((()())())"  =>  true
+#
+#
+## print(valid_parentheses("hi(hi)("))
+## print(valid_parentheses("hi(hi)()()()"))
+
 def parentheses(a):
 	counter1=0
 	counter2=0
